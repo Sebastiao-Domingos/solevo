0ffa11aed20a9d9e72bda6aba434fae7c0e3eb0f
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2023-12-18 22:23:02 +0100

    adicionando o menu inacabado

--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -1,4 +1,39 @@
+import Link from "next/link";
 import React from "react";
+
+
+const links = [
+    {
+        group : "Markiting",
+        menu : {
+            title : "Home",
+            link : "#",
+            icon : "",
+        },
+        submenus : [
+            {
+                title : "Fazendas",
+                link : "#",
+                icon : "",
+            },
+            {
+                title : "Produtos",
+                link : "#",
+                icon : "",
+            },
+            {
+                title : "Proprietarios",
+                link : "#",
+                icon : "",
+            },
+            {
+                title : "Markiting",
+                link : "#",
+                icon : "",
+            },
+        ]
+    }
+]
 
 
 export default function Layout({children} : {children : React.ReactNode}){
@@ -9,10 +44,7 @@ export default function Layout({children} : {children : React.ReactNode}){
                     <h2 className="text-amber-600 font-bold text-2xl">Fazendas</h2>
                     <button className="p-2 rounded shadow text-xl"><i className="ri-arrow-left-s-line"></i></button>
                 </div>
-                
-                <nav className="">
-                    Menu verical
-                </nav>
+                <Menu></Menu>
             </div>
 
             <div className="ml-[300px]">
@@ -29,5 +61,33 @@ export default function Layout({children} : {children : React.ReactNode}){
                 </div>
             </div>
         </div>
+    )
+}
+
+
+
+function Menu(){
+    return (
+        <nav>
+            { links.map( ( link , index ) => (
+                <div className="w-full">
+                <h3 className="mb-4 ">{link.group}</h3>    
+                <ul className="w-full flex flex-col gap-3 text-xl">
+                    <li className="w-full flex">
+                        <Link className="w-full p-3 hover:bg-amber-600/40 rounded" href={link.menu.link}>{link.menu.title}</Link>
+                    </li>
+                    <li className="w-full flex">
+                        <Link className="w-full p-3 hover:bg-amber-600/40 rounded" href={"#"}>Fazendas</Link>
+                    </li>
+                    <li className="w-full flex">
+                        <Link className="w-full p-3 hover:bg-amber-600/40 rounded" href={"#"}>Produtos</Link>
+                    </li>
+                    <li className="w-full flex">
+                        <Link className="w-full p-3 hover:bg-amber-600/40 rounded" href={"#"}>Proprientarios</Link>
+                    </li>
+                </ul>
+                </div>
+            ))}
+        </nav>
     )
 }
