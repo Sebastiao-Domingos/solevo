082b557bf9059b1ed53f8e4bc60fc0ae7ff295f8
Sebastiao-Domingos <domingossebastiao380@gmail.com>
2023-12-18 21:38:52 +0100

    adicionando afuncionabilidade de mostrar e esconder a pass word

--- src/app/(admin)/login/page.tsx
+++ src/app/(admin)/login/page.tsx
@@ -1,18 +1,22 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
 
 
 function Page(){
+    const [shadow , setShow ] = useState(false);
+
     return (
         <div className="w-full h-screen flex items-center justify-center">
             <div className="flex justify-between w-[80%]">
                 <div className="w-[50%] flex">
                     <div className="m-auto space-y-5">
-                        <h2 className="text-4xl font-bold text-amber-800 pl-2 border-l-2">Sistema de gestão de Fazendas</h2>
+                        <h2 className="text-4xl font-bold text-amber-600">Sistema de gestão de Fazendas</h2>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci odio facere quia itaque eligendi quis! Dolores illum veniam dolore tenetur officia, dolorem dolorum non laboriosam nobis consequatur? Necessitatibus, quo hic.</p>
                     </div>
                 </div>
                 <div className="w-[48%] min-h-[25rem] rounded shadow-sm px-4 py-8 space-y-10">
-                    <h2 className="w-full text-center text-4xl font-bold text-amber-800">Log In</h2>
+                    <h2 className="w-full text-center text-4xl font-bold text-amber-600">Log In</h2>
                     <form action="" className="flex flex-col gap-5">
                         <div className="w-full flex flex-col gap-3">
                             <label className="text-xl" htmlFor="email">Email <span className="text-red-500">*</span></label>
@@ -20,11 +24,21 @@ function Page(){
                                placeholder="Email do Administrador"
                              />
                         </div>
-                        <div className="w-full flex flex-col gap-3">
+                        <div className="relative w-full flex flex-col gap-3">
                             <label className="text-xl" htmlFor="pass">Palavra passe <span className="text-red-500">*</span></label>
-                            <input className="p-3 text-xl outline-none border rounded" type="password" name="pass" id="pass" 
+                            <input className="p-3 text-xl outline-none border rounded" type= { !shadow ? "password" : "text"} name="pass" id="pass" 
                                placeholder="Palavra passe"
                             />
+                            <button className="absolute bottom-3 right-3"
+                               onClick={ (e) => {
+                                e.preventDefault()
+                                setShow( res => !res )
+                               }}
+                            >
+                                {shadow && <i className="ri-eye-line"></i>}
+                                {!shadow && <i className="ri-eye-2-line"></i>}
+                                
+                            </button>
                         </div>
 
                         <div className="space-x-7 mt-8">
